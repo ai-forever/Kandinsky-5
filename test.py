6757e7a450589d875215bc4210e50948ca33d250
MarKovka20 <kovaleva.md@phystech.edu>
2025-09-20 00:12:12 +0300

    add local-rank in test

--- test.py
+++ test.py
@@ -3,31 +3,42 @@ import time
 
 from kandinsky import get_T2V_pipeline
 
+
 def validate_args(args):
     size = (args.width, args.height)
     supported_sizes = [(512, 512), (512, 768), (768, 512)]
     if not size in supported_sizes:
-        raise NotImplementedError(f"Provided size of video is not supported: {size}")
+        raise NotImplementedError(
+            f"Provided size of video is not supported: {size}")
+
 
 def parse_args():
     parser = argparse.ArgumentParser(
         description="Generate a video using Kandinsky 5"
     )
     parser.add_argument(
+        '--local-rank',
+        type=int,
+        help='local rank'
+    )
+    parser.add_argument(
         "--config",
         type=str,
         default="./configs/config_5s_sft.yaml",
-        help="The config file of the model")
+        help="The config file of the model"
+    )
     parser.add_argument(
         "--prompt",
         type=str,
         default="a cat in a blue hat",
-        help="The prompt to generate video")
+        help="The prompt to generate video"
+    )
     parser.add_argument(
         "--negative_prompt",
         type=str,
         default="Static, 2D cartoon, cartoon, 2d animation, paintings, images, worst quality, low quality, ugly, deformed, walking backwards",
-        help="Negative prompt for classifier-free guidance")
+        help="Negative prompt for classifier-free guidance"
+    )
     parser.add_argument(
         "--width",
         type=int,
@@ -52,33 +63,43 @@ def parse_args():
         "--expand_prompt",
         type=int,
         default=1,
-        help="Whether to use prompt expansion.")
+        help="Whether to use prompt expansion."
+    )
     parser.add_argument(
-        "--sample_steps", type=int, default=50, help="The sampling steps number.")
+        "--sample_steps",
+        type=int,
+        default=50,
+        help="The sampling steps number."
+    )
     parser.add_argument(
         "--guidance_weight",
         type=float,
         default=5.0,
-        help="Guidance weight.")
+        help="Guidance weight."
+    )
     parser.add_argument(
         "--scheduler_scale",
         type=float,
         default=10.0,
-        help="Scheduler scale.")
+        help="Scheduler scale."
+    )
     parser.add_argument(
         "--output_filename",
         type=str,
         default=None,
-        help="Name of the resulting file")
+        help="Name of the resulting file"
+    )
     args = parser.parse_args()
     return args
+
 
 if __name__ == "__main__":
     args = parse_args()
     validate_args(args)
-    
+
     pipe = get_T2V_pipeline(
-        device_map={"dit": "cuda:0", "vae": "cuda:0", "text_embedder": "cuda:0" },
+        device_map={"dit": "cuda:0", "vae": "cuda:0",
+                    "text_embedder": "cuda:0"},
         conf_path=args.config
     )
 
