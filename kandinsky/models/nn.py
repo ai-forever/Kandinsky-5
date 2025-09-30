d6b5cb635f19d829e3154387a83c5997699be445
MarKovka20 <60795929+MarKovka20@users.noreply.github.com>
2025-09-30 12:19:16 +0300

    fix for FA2

--- kandinsky/models/nn.py
+++ kandinsky/models/nn.py
@@ -15,7 +15,7 @@ if torch.cuda.get_device_capability()[0] >= 9:
     try:
         from flash_attn_interface import flash_attn_func as FA
     except:
-        FA = None
+        FA = FA
 else:
     try:
         from flash_attn import flash_attn_func as FA
